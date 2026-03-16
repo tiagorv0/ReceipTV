378e580e38f045fd61bb8f330936c9a3ebb0d5e9
Tiago Reolon Vazzoller <tiago_rv0@hotmail.com>
2026-03-16 16:09:02 -0300

    fix: database connection pool ssl

--- server/config/database.js
+++ server/config/database.js
@@ -6,9 +6,9 @@ const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  max: 10,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  ssl: { rejectUnauthorized: false },
+  // desativa prepared statements para funcionar com pgbouncer
+  statement_timeout: 10000,
 });
 
 export default pool;
